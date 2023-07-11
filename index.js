72e7931609263ffad662bc45c4f7b448f32f83a7
Nigora Bobayarova <57304108+nigora8@users.noreply.github.com>
2023-07-11 01:05:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function updateClock(){
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondsEl.innerText = s;
-    ampmEl.innerText = ampm;
+    ampmEl,(innerText = ampm);
     setTimeout(()=>{
         updateClock();
 
